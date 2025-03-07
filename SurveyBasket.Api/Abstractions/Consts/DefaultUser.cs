e02123a8a51a6340f6f1d3244f79e9791366d13f
Mahmoud Taha <tmahmoud311@gmail.com>
2025-03-07 06:00:12 +0200

    Add fluent validation

---
+++ SurveyBasket.Api/Abstractions/Consts/DefaultUser.cs
@@ -0,0 +1,15 @@
+﻿namespace SurveyBasket.Api.Abstractions;
+
+public static class DefaultUser
+{
+
+
+    public partial class Admin
+    {
+        public const string Id = "0bfe410c-6723-4bc5-b163-7469b0b8cb35";
+        public const string Email = "Admin@survey-Basket.com";
+        public const string Password = "AQAAAAIAAYagAAAAECiYs5sHmjOKUm9Kxgn2sO0gfzFfInNmKeIyfnSQdOuE1hB2SzucNKQ9AwOIyWeTDQ==";
+        public const string SecurityStamp = "75a8ae76fbd443f99afcfd3883674e06";
+        public const string ConcurrencyStamp = "c2a24c0c-6e74-4bce-858e-b190dbc77025";
+    }
+}
